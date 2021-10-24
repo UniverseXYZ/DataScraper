66e1e0f823abe5a0736c10664d080b4e819102e7
Ryan Shtirmer <rshtirmer@gmail.com>
2021-10-24 17:48:22 -0400

    Dockerize into EventMonitor, API, and PG (#2)
    * ugly init
    
    * improve db
    
    * add express api
    
    * add transfer endpoints for reading data

---
+++ API/index.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const { Pool } = require('pg')
+const connectionString = process.env.DB_URI
+const pool = new Pool({ connectionString })
+const app = express()
+const port = 3000
+
+app.get("/transfers", async (req, res) => {
+    try {
+      const allTransfers = await pool.query("SELECT * FROM transfers ORDER BY id DESC limit 200");
+      res.json(allTransfers.rows);
+    } catch (err) {
+      console.error(err.message);
+    }
+});
+  
+app.get("/transfers/from/:addr", async (req, res) => {
+    try {
+      const { addr } = req.params;
+      const transfers = await pool.query("SELECT * FROM transfers WHERE fromAddr = $1 ORDER BY id DESC limit 200", [
+        addr
+      ]);
+  
+      res.json(transfers.rows);
+    } catch (err) {
+      console.error(err.message);
+    }
+});
+
+app.get("/transfers/to/:addr", async (req, res) => {
+    try {
+      const { addr } = req.params;
+      const transfers = await pool.query("SELECT * FROM transfers WHERE toAddr = $1 ORDER BY id DESC limit 200", [
+        addr
+      ]);
+  
+      res.json(transfers.rows);
+    } catch (err) {
+      console.error(err.message);
+    }
+});
+
+
+app.get("/transfers/contract/:addr", async (req, res) => {
+    try {
+      const { addr } = req.params;
+      const transfers = await pool.query("SELECT * FROM transfers WHERE tokenAddr = $1 ORDER BY id DESC limit 200", [
+        addr
+      ]);
+  
+      res.json(transfers.rows);
+    } catch (err) {
+      console.error(err.message);
+    }
+});
+
+app.get("/transfers/token/:addr/:id", async (req, res) => {
+    try {
+      const { addr, id } = req.params;
+      const transfers = await pool.query("SELECT * FROM transfers WHERE tokenAddr = $1 AND tokenId = $2 ORDER BY id DESC limit 200", [
+        addr, id
+      ]);
+      res.json(transfers.rows);
+    } catch (err) {
+      console.error(err.message);
+    }
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ EventMonitor/index.js
@@ -0,0 +1,70 @@
+const { provider } = require('./getProvider')
+const { getAllTransfers } = require('./eventScraper')
+const { Pool } = require('pg')
+require('dotenv').config()
+
+const connectionString = process.env.DB_URI
+const pool = new Pool({ connectionString })
+let client
+
+async function initDB(drop=false){
+    if(drop) await client.query('DROP TABLE IF EXISTS transfers')
+    await client.query(`
+    CREATE TABLE IF NOT EXISTS transfers (
+        id SERIAL,
+        data jsonb NOT NULL,
+        fromAddr    TEXT GENERATED ALWAYS AS (data ->> 'from') stored,
+        toAddr      TEXT GENERATED ALWAYS AS (data ->> 'to')   stored,
+        tokenAddr   TEXT GENERATED ALWAYS AS (data ->> 'address') stored,
+        tokenId     varchar(200) GENERATED ALWAYS AS (data ->> 'tokenId') stored,
+        amount      TEXT GENERATED ALWAYS AS (data ->> 'amount') stored,
+        logIndex    TEXT GENERATED ALWAYS AS (data ->> 'logIndex') stored,
+        txHash      TEXT GENERATED ALWAYS AS (data ->> 'transactionHash') stored,
+        PRIMARY     KEY (txHash, logIndex, tokenId)
+    );`
+    )
+}
+
+async function storeTransfers(transfers){
+    for(let i=0; i<transfers.length;i++) {  
+        let t = transfers[i]
+        if(t.removed) continue;
+        delete t.data;  
+        delete t.topics;  
+        delete t.removed;  
+        delete t.blockHash;
+        try{
+            await client.query('INSERT INTO transfers (data) VALUES ($1);', [JSON.stringify(t)])
+        } catch (e) {}
+    }
+}
+
+async function scrape(blockNumber){
+    blockNumber = blockNumber - 6
+    console.log("Block: %d", blockNumber)
+    transfers = await getAllTransfers(blockNumber)
+    if(transfers.length > 0){
+        console.log("Found %d transfers", transfers.length)
+        //console.log("Preview: ", transfers[0])
+        await storeTransfers(transfers)
+    } else {
+        console.log("No transfers found on block: %d", blockNumber)
+    }
+    let result = await client.query('SELECT * FROM transfers;')
+    console.log("%d rows in transfers", result.rows.length)
+    //console.log(result.rows[result.rows.length-1])
+}
+/*
+provider.getBlockNumber().then(blockNumber => {
+    main(blockNumber)
+})
+*/
+async function main(){
+    client = await pool.connect()
+    await initDB(false)
+    provider.on("block", (blockNumber) => {
+        scrape(blockNumber)
+    })    
+}
+
+main()
